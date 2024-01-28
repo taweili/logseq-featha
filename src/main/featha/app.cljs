6443d79c1315467e65b47a0ed0edaa41b8412c86
David Li <taweili@gmail.com>
2024-01-28 11:41:07 +0800

    No external library. Use clojurescript native to deal with promise.

--- src/main/featha/app.cljs
+++ src/main/featha/app.cljs
@@ -1,13 +1,12 @@
 (ns featha.app
   (:require
-   ["@logseq/libs"]
-   [promesa.core :as p]))
+   ["@logseq/libs"]))
 
 (defn main []
   (js/console.log "Hello from featha main")
   (js/logseq.App.showMsg "❤️  Message from featha main"))
 
 (defn init []
-  (-> (p/promise (js/logseq.ready))
-      (p/then main)
-      (p/catch #(js/console.error))))
+  (-> (js/logseq.ready)
+      (.then main)
+      (.catch #(js/console.error))))
