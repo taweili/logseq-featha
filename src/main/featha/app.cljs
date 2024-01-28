9c4ed7db566a0b89162b69a5bfe0ea71a1cabc35
David Li <taweili@gmail.com>
2024-01-28 19:16:02 +0800

    Add example of slash command and menu action

--- src/main/featha/app.cljs
+++ src/main/featha/app.cljs
@@ -2,11 +2,19 @@
   (:require
    ["@logseq/libs"]))
 
+(defn slash-command [] 
+  (js/console.log "Hello from featha slash-command"))
+
+(defn block-context-menu-item-action []
+  (js/console.log "Hello from featha block-context-menu-item-action"))
+
 (defn main []
   (js/console.log "Hello from featha main")
-  (js/logseq.App.showMsg "❤️  Message from featha main"))
+  (js/logseq.App.showMsg "❤️  Message from featha main")
+  (js/logseq.Editor.registerSlashCommand "featha" slash-command)
+  (js/logseq.Editor.registerBlockContextMenuItem "featha" block-context-menu-item-action))
 
 (defn init []
-  (-> (js/logseq.ready)
-      (.then main)
+  (-> (js/logseq.ready) ; return a promise
+      (.then main)      ; then on the promise
       (.catch #(js/console.error))))
