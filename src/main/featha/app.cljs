c56f0c029aac7741177f6b67267bcc2177b1e6eb
David Li <taweili@gmail.com>
2024-01-28 20:22:18 +0800

    add comments

--- src/main/featha/app.cljs
+++ src/main/featha/app.cljs
@@ -3,16 +3,23 @@
    ["@logseq/libs"]))
 
 (defn slash-command [] 
-  (js/console.log "Hello from featha slash-command"))
+  (js/console.log "Hello from slash-command"))
 
-(defn block-context-menu-item-action []
-  (js/console.log "Hello from featha block-context-menu-item-action"))
+; use ^js type-hint to access js properties, 
+; in this case, keep the compiler happy to remove the warning 
+; "Cannot infer target type in expression (. e -uuid)"
+(defn block-context-menu-item-action [^js/IHookEvent e]
+  (js/console.log "Hello from block-context-menu-item-action %s" (.-uuid e)))
 
 (defn main []
+  ; print to console
   (js/console.log "Hello from featha main")
+  ; show message in logseq UI
   (js/logseq.App.showMsg "❤️  Message from featha main")
+  ; register slash command
   (js/logseq.Editor.registerSlashCommand "featha" slash-command)
-  (js/logseq.Editor.registerBlockContextMenuItem "featha" block-context-menu-item-action))
+  ; register block context menu item use fn.
+  (js/logseq.Editor.registerBlockContextMenuItem "featha" (fn [e] (block-context-menu-item-action e))))
 
 (defn init []
   (-> (js/logseq.ready) ; return a promise
