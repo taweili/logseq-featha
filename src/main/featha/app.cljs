7b5b33cb228e3987b26ee5eee9f310bbc09082d5
David Li <taweili@gmail.com>
2024-01-30 21:26:28 +0800

    Interact with Promise without external library

--- src/main/featha/app.cljs
+++ src/main/featha/app.cljs
@@ -2,14 +2,30 @@
   (:require
    ["@logseq/libs"]))
 
-(defn slash-command [] 
+(defn slash-command []
   (js/console.log "Hello from slash-command"))
+
+
+;
+; Interact with Promise without external library
+;
 
 ; use ^js type-hint to access js properties, 
 ; in this case, keep the compiler happy to remove the warning 
 ; "Cannot infer target type in expression (. e -uuid)"
 (defn block-context-menu-item-action [^js/IHookEvent e]
   (js/console.log "Hello from block-context-menu-item-action %s" (.-uuid e)))
+
+(defn get-page [^js/BlockEntity block]
+  (let [page_id (.-page block)]
+    (js/console.log block)
+    (-> (js/logseq.Editor.getPage (.-id page_id))
+        (.then js/console.log))))
+
+; use core.async to make sure async codes are executed.
+(defn block-context-menu-get-all-blocks-on-the-page [^js/IHookEvent e]
+    (-> (js/logseq.Editor.getBlock (.-uuid e))
+        (.then get-page)))
 
 (defn main []
   ; print to console
@@ -19,7 +35,9 @@
   ; register slash command
   (js/logseq.Editor.registerSlashCommand "featha" slash-command)
   ; register block context menu item use fn.
-  (js/logseq.Editor.registerBlockContextMenuItem "featha" (fn [e] (block-context-menu-item-action e))))
+  (js/logseq.Editor.registerBlockContextMenuItem "featha" (fn [e] (block-context-menu-item-action e)))
+  ; register block context menu to process block
+  (js/logseq.Editor.registerBlockContextMenuItem "featha (block)" (fn [e] (block-context-menu-get-all-blocks-on-the-page e))))
 
 (defn init []
   (-> (js/logseq.ready) ; return a promise
